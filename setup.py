985048482689181a9775bddc0f8b588fa5d7f5b2
Lucas Hoffmann <l-m-h@web.de>
2015-02-25 13:29:57 +0100

    Replace script by "entry_points" keyword to setuptools.setup().

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     name=constants.name,
     author='Lucas Hoffmann',
     version='.'.join(map(str, constants.version)),
-    scripts=['scripts/manga-dl'],
     packages=['manga_dl'],
+    entry_points={'console_scripts': ['manga-dl = manga_dl.cli:main']}
 )
