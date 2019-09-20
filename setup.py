66275cd2ab3bdb41d60ce670449adf7fead1331b
Lucas Hoffmann <lucc@posteo.de>
2019-09-20 20:10:27 +0200

    Update setup file for rewritten script

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup
 import manga_dl
 
 setup(
-    name=manga_dl.name,
+    name='comic-dl',
     author='Lucas Hoffmann',
-    version='.'.join(map(str, manga_dl.__version__)),
-    packages=['manga_dl'],
-    entry_points={'console_scripts': ['manga-dl = manga_dl.cli:main']}
+    scripts=['comic-dl.py'],
+    version='0.5-dev',
 )
