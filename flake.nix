65c78c42693e1dc4886874a8bc3051aa46d1eef3
Lucas Hoffmann <lucc@posteo.de>
2024-08-02 08:19:26 +0200

    Remove unused derivation from flake
    The download-all script is superseded by the resume subcommand of the
    main python script.

--- flake.nix
+++ flake.nix
@@ -6,30 +6,21 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    pythonPackages = pkgs.python3Packages;
+    pyPkgs = pkgs.python3Packages;
     pyproject = (pkgs.lib.trivial.importTOML ./pyproject.toml).project;
+    names = pyproject.dependencies;
+    packages = builtins.attrValues (pkgs.lib.attrsets.getAttrs names pyPkgs);
+    deps = [ pyPkgs.setuptools pyPkgs.setuptools-scm ] ++ packages;
+    typing-deps = deps ++ [ pyPkgs.mypy pyPkgs.types-beautifulsoup4 ];
   in
   {
-    packages.x86_64-linux = rec {
-
-      default = pythonPackages.buildPythonApplication {
-        name = "comic-dl";
-        version = pyproject.version;
-        pyproject = true;
-        src = ./.;
-        propagatedBuildInputs = let
-          names = pyproject.dependencies;
-          packages = builtins.attrValues (pkgs.lib.attrsets.getAttrs names pythonPackages);
-        in [ pythonPackages.setuptools pythonPackages.setuptools-scm ] ++ packages;
-        checkPhase = "python -m unittest";
-      };
-
-      download-all = pkgs.writeShellScriptBin "download-all" ''
-        dir=''${1?You have to provide a directory as first argument.}
-        for f in "$dir"/*/state.pickle; do
-          ${default}/bin/comic-dl download --directory "''${f%/state.pickle}" --resume
-        done
-      '';
+    packages.x86_64-linux.default = pyPkgs.buildPythonApplication {
+      name = "comic-dl";
+      version = pyproject.version;
+      pyproject = true;
+      src = ./.;
+      propagatedBuildInputs = deps;
+      checkPhase = "python -m unittest";
     };
 
     checks.x86_64-linux = {
